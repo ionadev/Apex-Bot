3c03dbbd04fd77f39638044dfb2fd69f5a8d4aa8
Paranjpay <soumil.paranjpay@gmail.com>
2018-06-08 22:58:02 +0530

    update command

---
+++ commands/Admin/update.js.js
@@ -0,0 +1,24 @@
+const { Command, util } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            permissionLevel: 10,
+            description: 'Updates the bot from gitlab.',
+        });
+    }
+
+    async run(message) {
+            const { stdout, stderr } = util.exec('git pull https://gitlab.com/stitch07/apex-bot');
+            if (err) return console.error(err);
+            const out = [];
+            if (stdout) out.push(stdout);
+            if (stderr) out.push(stderr);
+            await message.channel.send(out.join('---\n'), { code: true });
+            if (!stdout.toString().includes('Already up-to-date.')) {
+              this.client.commands.get('reboot').run(message);
+            }
+        }
+    }
+};
