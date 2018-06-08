e328d849882ab8d9c98793407f8a1894b17c62e2
Paranjpay <soumil.paranjpay@gmail.com>
2018-06-08 22:42:24 +0530

    fix

--- events/ready.js
+++ events/ready.js
@@ -4,13 +4,11 @@ const config = require('../config');
 module.exports = class extends Event {
 
 	constructor(...args) {
-		super(...args, { 
-			enabled: true
-		});
+		super(...args, { enabled: true });
 	}
 
 	run() {
-		this.client.commands.get('conf').permissionLevel = 3 
+		this.client.commands.get('conf').permissionLevel = 3;
 		this.client.user.setActivity(`${config.PRODUCTION ? config.PRODUCTIONPREFIX : config.DEVPREFIX}help | ${this.client.guilds.size} servers`);
 	}
 
