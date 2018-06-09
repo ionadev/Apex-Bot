b382b6d3494812da53d12679e5d7b65fa2a7492f
Paranjpay <soumil.paranjpay@gmail.com>
2018-06-09 22:52:34 +0530

    bugfixes

--- events/ready.js
+++ events/ready.js
@@ -4,7 +4,7 @@ const config = require('../config');
 module.exports = class extends Event {
 
 	async run() {
-		if (!await this.r.dbList().run().includes('apex')) await this.r.dbCreate('apex').run();
+		if (!(await this.r.dbList().run()).includes('apex')) await this.r.dbCreate('apex').run();
 		this.client.commands.get('conf').permissionLevel = 3;
 		this.client.user.setActivity(`${config.PRODUCTION ? config.PRODUCTIONPREFIX : config.DEVPREFIX}help | ${this.client.guilds.size} servers`);
 	}
