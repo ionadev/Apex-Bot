83f347300989936d8fa26d481bb8ed06f264aeaf
Paranjpay <soumil.paranjpay@gmail.com>
2018-06-10 11:33:39 +0530

    fix

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -18,17 +18,17 @@ module.exports = class extends Command {
 	async run(msg, [query]) {
 		let player = Player.getPlayer(msg.guild.id);
 		if (!player) player = new Player(this.client, this.client._player, msg.guild)
-		player.textChannelID = msg.channel.id
+		player.textChannelID = msg.channel.id;
 		if ('soundcloud' in msg.flags) query = `scsearch:${query}`;
 		let item;
 		if ('choose' in msg.flags) {
-			item = await player.add(query, async songs => {
+			item = await player.add(msg.author, query, async songs => {
 				prompt(this, songs
 					.map(({ author, title }, i) => `${i + 1}). ${title} \`${author}\``)
 					.join('\n'));
 			});
 		} else {
-			item = await player.add(query);
+			item = await player.add(msg.author, query);
 		}
 		const musicPlayer = this.client._player.join({
 			guild: msg.guild.id,
@@ -36,7 +36,7 @@ module.exports = class extends Command {
 			host: this.client._player.nodes.first().host
 		});
 		if (!musicPlayer.playing) player.play(musicPlayer);
-		if (Array.isArray(item)) return msg.send(`Queued a total **${item.length} songs.`);
+		if (Array.isArray(item)) return msg.send(`Added **${item.length} songs to the queue`);
 		return msg.send(`Added **${item.title}** to the queue.`);
 	}
 
--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -18,7 +18,7 @@ module.exports = class extends Command {
 		const { queue } = player;
 		const output = [];
 		for (let i = 0; i < Math.min(queue.length, 10); i++) {
-			output.push(`${i + 1}. ${queue[i].title} - Requested by: ${queue[i].requester.tag || 'Youtube Autoplay'} (${showSeconds(queue[i].seconds * 1000)})`);
+			output.push(`${i + 1}. ${queue[i].title} - Requested by: ${queue[i].requester.tag || 'Youtube Autoplay'}`);
 		}
 		return msg.channel.send([
 			`🗒 __**${msg.guild.name}'s Music Queue:**__ Currently **${queue.length}** songs queued ${(queue.length > 10 ? '*[Only next 10 shown]*' : '')}`,
