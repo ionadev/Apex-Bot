9ac56c182f2ccfab397ece4d6c160c8b474f02f0
Paranjpay <soumil.paranjpay@gmail.com>
2018-06-12 20:48:46 +0530

    more work

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -1,4 +1,5 @@
 const { Command } = require('klasa');
+const Player = require('../../lib/Player');
 module.exports = class extends Command {
 
 	constructor(...args) {
@@ -9,21 +10,28 @@ module.exports = class extends Command {
 			promptLimit: 0,
 			permissionLevel: 0,
 			description: 'Adds a song or plays the music queue.',
-			usage: '<query:string>'
+			usage: '[query:string]'
 		});
 	}
 
 	async run(msg, [query]) {
 		const { player } = msg.guild;
+		if (!query) {
+			if (!player.player.playing) return msg.send('The dispatcher is already playing a track.');
+			if (!player.player.paused) return this.client.commands.get('resume').run(msg);
+			return null;
+		}
 		player.textChannelID = msg.channel.id;
 		if ('soundcloud' in msg.flags) query = `scsearch:${query}`;
 		let item;
 		if ('choose' in msg.flags) {
-			item = await player.add(msg.author, query, async songs => {
-				prompt(this, songs
-					.map(({ author, title }, i) => `${i + 1}). ${title} \`${author}\``)
-					.join('\n'));
-			});
+			item = await player.add(msg.author, query, true);
+			if (item.length === 1) {
+				await msg.channel.send(`Found only one selection: **${item[0].title}**`);
+				[item] = item;
+			} else {
+				item = await prompt(item, msg);
+			}
 		} else {
 			item = await player.add(msg.author, query);
 		}
@@ -35,10 +43,21 @@ module.exports = class extends Command {
 			host: this.client._player.nodes.first().host
 		});
 		if (!musicPlayer.playing) player.play(musicPlayer);
+		return null;
 	}
 
 };
 
-function prompt() {
-	// noop
+async function prompt(songs, msg) {
+	songs = songs.slice(0, 5);
+	await msg.channel.send([`🎵 **Top 5 searches**, choose a selection:\n`,
+		`${songs.map((song, i) => `${i + 1}. [${song.title}](${song.uri}) by ${song.author} - ${Player.showSeconds(song.length)}`)}`
+	].join('\n'));
+	const messages = await msg.channel.awaitMessages(message => message.author === msg.author && parseInt(message.content));
+	if (!messages || !messages.size) throw 'No selection chosen: aborting prompt.';
+	const selection = messages.first().content;
+	const vid = parseInt(selection);
+	if (vid < 1 || vid > 5) throw 'Invalid option: expected a number between 1 and 5.';
+	return songs[vid - 1];
 }
+
