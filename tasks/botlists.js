92b025ec41b125d7f445cecf60c70d4144190702
Soumil07 <soumil.paranjpay@gmail.com>
2018-07-04 08:33:32 +0530

    add botlist update task

---
+++ tasks/botlists.js
@@ -0,0 +1,31 @@
+/* eslint-disable camelcase */
+
+const { Task } = require('klasa');
+const snek = require('snekfetch');
+const config = require('../config');
+
+module.exports = class extends Task {
+
+	async run() {
+		return Promise.all([this.updateDBL(), this.updateDBots()]);
+	}
+
+	async updateDBL() {
+		return snek
+			.get(`https://discordbots.org/api/bots/${this.client.user.id}`)
+			.set('Authorization', config.BOTLISTS.DBL)
+			.send({ server_count: this.client.guilds.size })
+			.then(() => this.client.emit('log', 'Posted stats to DBL.'))
+			.catch(() => null);
+	}
+
+	async updateDBots() {
+		return snek
+			.get(`https://bots.discord.pw/api/bots/${this.client.user.id}`)
+			.set('Authorization', config.BOTLISTS.DBOTS)
+			.send({ server_count: this.client.guilds.size })
+			.then(() => this.client.emit('log', 'Posted stats to Dbots.'))
+			.catch(() => null);
+	}
+
+};
