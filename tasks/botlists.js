9885957ae8f3990c1a2e89b777c379eaed98a3b7
Paranjpay <soumil.paranjpay@gmail.com>
2018-07-04 21:57:42 +0530

    add BFD updater

--- tasks/botlists.js
+++ tasks/botlists.js
@@ -7,7 +7,7 @@ const config = require('../config');
 module.exports = class extends Task {
 
 	async run() {
-		return Promise.all([this.updateDBL(), this.updateDBots()]);
+		return Promise.all([this.updateDBL(), this.updateDBots(), this.updateBFD()]);
 	}
 
 	async updateDBL() {
@@ -23,6 +23,15 @@ module.exports = class extends Task {
 		return snek
 			.post(`https://bots.discord.pw/api/bots/${this.client.user.id}/stats`)
 			.set('Authorization', config.BOTLISTS.DBOTS)
+			.send({ server_count: this.client.guilds.size })
+			.then(() => this.client.emit('log', 'Posted stats to Dbots.'))
+			.catch(() => null);
+	}
+
+	async updateBFD() {
+		return snek
+			.post(`https://botsfordiscord.com/api/v1/bots/${this.client.user.id}`)
+			.set('Authorization', config.BOTLISTS.BFD)
 			.send({ server_count: this.client.guilds.size })
 			.then(() => this.client.emit('log', 'Posted stats to Dbots.'))
 			.catch(() => null);
